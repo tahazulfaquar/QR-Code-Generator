dbcbac98b3d6ab69e81a73b7aa8e89e599c4a27f
tahazulfaquar <71271978+tahazulfaquar@users.noreply.github.com>
2021-10-09 19:16:32 +0530

    Optimized the code.

--- QRCodeGenerator.py
+++ QRCodeGenerator.py
@@ -1,11 +1,5 @@
 import pyqrcode 
 from pyqrcode import QRCode
-  
-# String which represent the QR code 
-s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
-  
-# Generate QR code 
+s = "https://github.com/TriCompany"
 url = pyqrcode.create(s) 
-  
-# Create and save the png file naming "myqr.png" 
 url.svg("myyoutube.svg", scale = 8) 
